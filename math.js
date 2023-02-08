0fa60c312d706cd1ae00cc6622230735093bbd05
marikrishnan47 <krishmarish028@gmail.com>
2023-02-08 20:41:31 +0530

    flie

---
+++ math.js
@@ -0,0 +1,96 @@
+
+	 
+	 
+// let month=prompt("Enter month")
+
+  // let date= new  Date()
+  // let dat= date.getMonth()
+  // console.log(dat)
+  
+  
+  // if(month>=0 && month<=3){
+	  // console.log("spring")
+  // }
+    // else if(month>=4 && month<=8){
+	  // console.log("summer")
+  // }
+     // else if(month>=9 && month<=11){
+	  // console.log("winnter")
+  // }
+  // else {
+	  // console.log("no")
+  // }
+  // let date=new Date();
+  // let times=date.toLocaleTimeString();
+  // console.log(times);
+  // let time=prompt("Enter Time")
+  
+  // if (time>=0 && time<12){
+	  
+	  // console.log("moring");
+	  
+  // }
+  // else if (time>=12 && time<=15){
+	  
+	  // console.log("afternoon");
+	  
+  // }
+    // else if (time>=16 && time<=18){
+	  
+	  // console.log("evening");
+	  
+  // }
+    // else if (time>=19 || time<=24){
+	  
+	  // console.log("night");
+	  
+  // }
+  // else{
+	  // console.log("no")
+  // }
+  
+  
+  
+  // let time= new Date()
+  // let a=time.getHours()
+  
+    // if (time>=0 && time<12){
+	  
+	  // console.log("moring");
+	  
+  // }
+  // else if (time>=12 && time<=15){
+	  
+	  // console.log("afternoon");
+	  
+  // }
+    // else if (time>=16 && time>=18){
+	  
+	  // console.log("evening");
+	  
+  // }
+    // else if (time>=19 || time>=24){
+	  
+	  // console.log("night");
+	  
+  // }
+  // else{
+	  // console.log("no")
+  // }
+  
+  
+  let month= new Date()
+  let moth=month.getMonth()
+  
+    if(month>=0 && month<=3){
+	  console.log("spring")
+  }
+    else if(month>=4 && month<=8){
+	  console.log("summer")
+  }
+     else if(month>=9 || month<=11){
+	  console.log("winnter")
+  }
+  else {
+	  console.log("no")
+  }
